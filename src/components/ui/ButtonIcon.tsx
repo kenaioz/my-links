0517c312a5c9048a02be6a50566293dd56035fcc
Silvio <silvio.cesarrx@hotmail.com>
2025-07-05 13:02:33 -0300

    improve transition

--- src/components/ui/ButtonIcon.tsx
+++ src/components/ui/ButtonIcon.tsx
@@ -3,7 +3,7 @@ function ButtonIcon({
 }: React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      className="hover:bg-foreground hover:text-background focus:bg-foreground focus:text-background cursor-pointer rounded-full p-1.5 transition-all focus:outline-0"
+      className="hover:bg-foreground hover:text-background focus:bg-foreground focus:text-background cursor-pointer rounded-full p-1.5 transition-all ease-in-out focus:outline-0"
       {...props}
     ></button>
   );
